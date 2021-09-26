486357567a45aaf331a049287bdd56ffd94b4653
David Osuna <davidosuna@macbook-pro-de-david.home>
2021-09-26 10:17:06 +0200

    Ranking

--- app/Http/Controllers/MinigameController.php
+++ app/Http/Controllers/MinigameController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Score;
 use App\Models\Minigame;
 use Illuminate\Http\Request;
 
@@ -23,67 +24,29 @@ class MinigameController extends Controller
 
     public function ranking($slug)
     {
-      return response()->json(['mnigame' => $slug, 'ranking' => [
-        // [
-        //   'ranking' => 1,
-        //   'score' => 9999,
-        //   'wallet' => '0xbE8fC9ff98CD894141F40c2f995E3C96d12aa1Ce',
-        //   'signature' => 'SIGNATURE0xbE8fC9ff98CD894141F40c2f995E3C96d12aa1Ce',
-        // ],
-        // [
-        //   'ranking' => 2,
-        //   'score' => 8999,
-        //   'wallet' => '0xbE8fC9ff98CD894141F40c2f995E3C96d12aa1Ce',
-        //   'signature' => 'SIGNATURE0xbE8fC9ff98CD894141F40c2f995E3C96d12aa1Ce',
-        // ],
-        // [
-        //   'ranking' => 3,
-        //   'score' => 7999,
-        //   'wallet' => '0xbE8fC9ff98CD894141F40c2f995E3C96d12aa1Ce',
-        //   'signature' => 'SIGNATURE0xbE8fC9ff98CD894141F40c2f995E3C96d12aa1Ce',
-        // ],
-        // [
-        //   'ranking' => 4,
-        //   'score' => 6999,
-        //   'wallet' => '0xbE8fC9ff98CD894141F40c2f995E3C96d12aa1Ce',
-        //   'signature' => 'SIGNATURE0xbE8fC9ff98CD894141F40c2f995E3C96d12aa1Ce',
-        // ],
-        // [
-        //   'ranking' => 5,
-        //   'score' => 5999,
-        //   'wallet' => '0xbE8fC9ff98CD894141F40c2f995E3C96d12aa1Ce',
-        //   'signature' => 'SIGNATURE0xbE8fC9ff98CD894141F40c2f995E3C96d12aa1Ce',
-        // ],
-        // [
-        //   'ranking' => 6,
-        //   'score' => 4999,
-        //   'wallet' => '0xbE8fC9ff98CD894141F40c2f995E3C96d12aa1Ce',
-        //   'signature' => 'SIGNATURE0xbE8fC9ff98CD894141F40c2f995E3C96d12aa1Ce',
-        // ],
-        // [
-        //   'ranking' => 7,
-        //   'score' => 3999,
-        //   'wallet' => '0xbE8fC9ff98CD894141F40c2f995E3C96d12aa1Ce',
-        //   'signature' => 'SIGNATURE0xbE8fC9ff98CD894141F40c2f995E3C96d12aa1Ce',
-        // ],
-        // [
-        //   'ranking' => 8,
-        //   'score' => 2999,
-        //   'wallet' => '0xbE8fC9ff98CD894141F40c2f995E3C96d12aa1Ce',
-        //   'signature' => 'SIGNATURE0xbE8fC9ff98CD894141F40c2f995E3C96d12aa1Ce',
-        // ],
-        // [
-        //   'ranking' => 9,
-        //   'score' => 1999,
-        //   'wallet' => '0xbE8fC9ff98CD894141F40c2f995E3C96d12aa1Ce',
-        //   'signature' => 'SIGNATURE0xbE8fC9ff98CD894141F40c2f995E3C96d12aa1Ce',
-        // ],
-        // [
-        //   'ranking' => 10,
-        //   'score' => 999,
-        //   'wallet' => '0xbE8fC9ff98CD894141F40c2f995E3C96d12aa1Ce',
-        //   'signature' => 'SIGNATURE0xbE8fC9ff98CD894141F40c2f995E3C96d12aa1Ce',
-        // ],
-      ]]);
+      $minigame = Minigame::whereSlug($slug)->first();
+      if(!$minigame) return response()->json(['success' => false, 'message' => 'Model not found']);
+      
+      $ranking_total = Score::whereMinigameId($minigame->id)->orderBy('score', 'desc')->limit(30)->get()->groupBy('wallet');
+      
+      $ranking = collect();
+      $index = 1;
+      foreach($ranking_total as $wallet => $scores):
+        $ranking->push([
+          'ranking' => $index,
+          'score' => $scores[0]->score,
+          'wallet' => $wallet,
+          'signature' => $wallet,
+        ]);
+
+        $index++;
+      endforeach;
+
+      return response()->json([
+        'success' => true,
+        'message' => 'Ranking for ' . $minigame->name . ' minigame',
+        'mnigame' => $slug,
+        'ranking' => $ranking
+      ]);
     }
 }
