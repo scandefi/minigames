cdf99f775f2071641b953312f6d2c68abe187db8
David Osuna <davidosuna@MacBook-Pro-de-David.local>
2021-10-04 17:23:43 +0200

    Round attrs

--- database/migrations/2021_10_04_084044_add_round_column_to_scores_table.php
+++ database/migrations/2021_10_04_084044_add_round_column_to_scores_table.php
@@ -15,11 +15,11 @@ class AddRoundColumnToScoresTable extends Migration
     public function up()
     {
         Schema::table('scores', function (Blueprint $table) {
-            $table->foreignId('round_id')->after('score')->constrained('rounds');
+            $table->foreignId('round_id')->constrained('rounds');
             $table->string('round_name')->after('round_id');
         });
 
-        DB::table('scores')->where('round_id', NULL)->update(['round_id' => 1, 'round_name' => '1']);
+        DB::table('scores')->where('round_id', 0)->update(['round_id' => 1, 'round_name' => '1']);
     }
 
     /**
